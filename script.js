bf0413fad3ac114af2c8afb1f21a59b5d9c36d79
thisisjao <128715989+thisisjao@users.noreply.github.com>
2024-05-28 08:48:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal= document.querrySelector(".caixa-principal");
+const caixaPerguntas= document.querrySelector(".caixa-perguntas");
+const caixaAlternativas= document.querrySelector(".caixa-alternativas");
+const caixaResultado= document.querrySelector(".caixa-resultado");
+const textoResultado= document.querrySelector(".texto-resultado");
 
+const perguntas= [
+  {
+      enunciado: "Pergunta1",
+      alternativas:[
+        "alternativa1",
+        "alternativa2",
+      ]
+  },
+   {
+      enunciado: "Pergunta2",
+      alternativas:[
+        "alternativa1",
+        "alternativa2",
+      ]
+  },
+   {
+      enunciado: "Pergunta3",
+      alternativas:[
+        "alternativa1",
+        "alternativa2",
+      ]
+  },
+   {
+      enunciado: "Pergunta4",
+      alternativas:[
+        "alternativa1",
+        "alternativa2",
+      ]
+  },
+   {
+      enunciado: "Pergunta5",
+      alternativas:[
+        "alternativa1",
+        "alternativa2",
+      ]
+  },
+  ];
